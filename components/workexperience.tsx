17f99728aa6edd094c104eea5867fc1ad2fb0a24
Felipe Macedo <fmacedo746@gmail.com>
2023-07-24 16:35:40 -0300

    fix: changing the size of the date and adding string padding on the left

--- components/workexperience.tsx
+++ components/workexperience.tsx
@@ -1,27 +1,29 @@
 import styles from '@/styles/Home.module.css';
 
-export default function WorkExperience(props: any) {
-    
+type work = {
+    url: string,
+    companyName: string,
+    period: {
+        start: { month: number, year: number },
+        end: { month: number, year: number }
+    }
+}
+
+export default function WorkExperience(props: work) {
+
     return (
         <a href={props.url}>
             <div className={styles.card}>
+
                 <div className={styles.projectname}>
                     <span>{props.companyName}</span>
-                    <div>
-                    <span>{props.period.start?.month}/{props.period.start?.year}</span>
+                </div>
+                <div style={{ display: "flex", gap: "10px" }}>
+                    <span>{props.period.start?.month.toString().padStart(2, "0")}/{props.period.start?.year}</span>
                     -
-                    <span>{props.period.end ? props.period.end?.month+"/"+props.period.end?.year : "now"}</span>
-                    </div>
-                    
+                    <span>{props.period.end ? props.period.end?.month.toString().padStart(2, "0") + "/" + props.period.end?.year : "now"}</span>
                 </div>
-                <div className={styles.projectdesc}>
-                    <p>{props.description}</p>
-                </div>
-                <div>
-                    <div>
-                    {props.techStack}
-                    </div>
-                </div>
+
             </div>
         </a>
     );
